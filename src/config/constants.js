44d520ffbb85217bc3dc612b501df9b59dbbabd5
Sangyeop Kim <yeop1108@naver.com>
2025-07-24 00:17:57 +0900

    api url 주소 변경

---
+++ src/config/constants.js
@@ -0,0 +1,4 @@
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://grab-market-server-yeop-damp-field-6492.fly.dev"
+    : "http://localhost:8080";
--- src/product/index.js
+++ src/product/index.js
@@ -1,8 +1,74 @@
+import "./index.css";
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { API_URL } from "../config/constants.js";
+import dayjs from "dayjs";
+import { Button, message } from "antd";
 
 function ProductPage() {
   const { id } = useParams();
-  return <h1>상품 상세 페이지 {id} 상품</h1>;
-}
+  const [product, setProduct] = useState(null);
 
+  const getProduct = () => {
+    axios
+      .get(`${API_URL}/products/${id}`)
+      .then(function (result) {
+        setProduct(result.data.product);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  useEffect(function () {
+    getProduct();
+  }, []);
+
+  if (product === null) {
+    return <h1>상품 정보를 받고 있습니다...</h1>;
+  }
+
+  const onClickPurchase = () => {
+    axios
+      .post(`${API_URL}/purchase/${id}`)
+      .then((result) => {
+        alert("구매가 완료되었습니다.");
+        getProduct();
+      })
+      .catch((error) => {
+        message.error(`에러가 발생했습니다. ${error.message}`);
+      });
+  };
+
+  return (
+    <div>
+      <div id="image-box">
+        <img src={`${API_URL}/${product.imageUrl}`} />
+      </div>
+      <div id="profile-box">
+        <img src="/images/icons/avatar.png" />
+        <span>{product.seller}</span>
+      </div>
+      <div id="contents-box">
+        <div id="name">{product.name}</div>
+        <div id="price">{product.price}</div>
+        <div id="createdAt">
+          {dayjs(product.createdAt).format("YYYY년 MM월 DD일")}
+        </div>
+        <Button
+          id="purchase-button"
+          size="large"
+          type="primary"
+          danger
+          onClick={onClickPurchase}
+          disabled={product.soldout === 1 ? true : false}
+        >
+          재빨리 구매하기
+        </Button>
+        <pre id="description">{product.description}</pre>
+      </div>
+    </div>
+  );
+}
 export default ProductPage;
